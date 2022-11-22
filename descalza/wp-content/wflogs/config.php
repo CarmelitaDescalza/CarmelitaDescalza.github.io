fb16c44ffef533d90c565f4c089888fff60ac0f6
Carmen Navarro Timón <armatedearte@gmail.com>
2022-11-22 11:43:02 +0100

    chaoDescalza

--- descalza/wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"]2(Q9~J 3@#<kAZ(eNF)X9:m#o}_Q>rec3=GSawp:Ne762Y1P,k;{~YDx?JFYIr!";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:2372;}
